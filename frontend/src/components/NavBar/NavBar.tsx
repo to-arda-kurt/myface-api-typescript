98a7018e7c7fe2508214bcec21ea60612e8499e5
Arda Kurt <to.arda.kurt@gmail.com>
2023-07-27 14:09:03 +0100

    Toggler Icons

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './Navbar.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHamburger, faXmark } from "@fortawesome/free-solid-svg-icons"
 
 function Navbar() {
   const [navbarExpanded, setNavbarExpanded] = useState(false)
@@ -9,7 +11,7 @@ function Navbar() {
     <nav className={`Navbar ${navbarExpanded ? "Navbar--expanded" : ""}`}>
       <NavLink to='/'><h3 className="Navbar__heading">MyFace</h3></NavLink>
       <div className="Navbar__content">
-        <button className="Navbar__toggler" onClick={() => setNavbarExpanded(!navbarExpanded)}>Expand</button>
+        <button className="Navbar__toggler" onClick={() => setNavbarExpanded(!navbarExpanded)}>{navbarExpanded ? <FontAwesomeIcon icon={faXmark}/>:<FontAwesomeIcon icon={faHamburger}/>}</button>
         <ul className="Navbar__links">
           <li><NavLink to="/posts" className={({isActive}) => `Navbar__link ${isActive ? "Navbar__link--active" : ""}`}>Posts</NavLink></li>
           <li><NavLink to="/users" className={({isActive}) => `Navbar__link ${isActive ? "Navbar__link--active" : ""}`}>Users</NavLink></li>
@@ -20,4 +22,4 @@ function Navbar() {
   )
 }
 
-export default Navbar
+export default Navbar;
